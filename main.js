6b925921b4f87288d8099e79a6df33ce58ced52f
ElaiasCZ <108530713+ElaiasCZ@users.noreply.github.com>
2024-03-06 12:37:49 -0300

    save

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-<<<<<<< HEAD
 const botoes = document .querySelectorAll(".botao");
+
+for(lei i=0;i <botoes.length;i++){
+   botoes[i].onclick = function(){
+    
+    for(let j=0;j<botoes.lenght;j++){
+        botoes[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+   }
+    console.log();
+}
