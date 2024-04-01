c371622ad9b35b2f0a4f8461719c85aad1d16955
ElaiasCZ <108530713+ElaiasCZ@users.noreply.github.com>
2024-04-01 11:11:52 -0300

    save

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll("aba.conteudo");
 
 for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+    botoes[i].onclick = function() {
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
@@ -16,6 +16,19 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-let tempoAtual = nem Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoFinal/ 1000);
+let minutos = Math.floor(segundos/ 60);
+let horas = Math.floor(minutos/ 60);
+let dias = Math.floor(horas/ 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
+}
