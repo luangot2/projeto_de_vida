dd4c0117ce7ec0167b81471418258fd066d7a1e0
ElaiasCZ <108530713+ElaiasCZ@users.noreply.github.com>
2024-04-03 12:18:53 -0300

    save

--- main.js
+++ main.js
@@ -15,7 +15,25 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-30T00:00:00");
+const tempoObjetivo3 = new Date("2024-11-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-30T00:00:00");
+
+const tempos =
+[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+   contadores[i].textContent = calculaTempo(tempos[i]); } 
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+
+const contadores = document.querySelectorAll(".contador");
+
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
@@ -27,8 +45,16 @@ let minutos = Math.floor(segundos/ 60);
 let horas = Math.floor(minutos/ 60);
 let dias = Math.floor(horas/ 24);
 
+comecaCronometro();
+
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
+if (tempoFinal > 0){
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else {
+    return "Prazo Finalizado";
 }
+}
+
